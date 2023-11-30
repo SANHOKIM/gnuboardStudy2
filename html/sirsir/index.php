c1262b69924e4be42017fa694a23aa1e62c31088
SANHOKIM <awrko@naver.com>
2023-11-30 12:09:43 +0900

    그누뜯2

--- html/sirsir/index.php
+++ html/sirsir/index.php
@@ -1,7 +1,9 @@
 <?php
-include_once('./_common.php');
+include_once('./_common.php'); // 모든 변수 상수 함수가 선언된 핵심 common.php를 호출함 
+                                      // _common : 언더바커먼의역할을 이해할 수 있다. 
 
-define('_INDEX_', true);
+
+define('_INDEX_', true); // 첫 페이지만 구분하는 상수변수선언
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 if(defined('G5_THEME_PATH')) {
